49893cf42d5925a203b6c6db196ce1f401217811
mdsumonfreeluncerbdapps1-prog <mdsumonfreeluncerbdapps1@gmail.com>
2026-04-12 02:36:06 +0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// SMS Listener
+app.post("/sms_listener", (req, res) => {
+
+console.log("SMS Received:", req.body);
+
+res.send("Welcome to Cricket Live Score Service");
+
+});
+
+// USSD Listener
+app.post("/ussd_listener", (req, res) => {
+
+console.log("USSD Request:", req.body);
+
+res.send("1. Live Match\n2. Bangladesh Match\n3. IPL Match");
+
+});
+
+// Subscription Listener
+app.post("/sub_listener", (req, res) => {
+
+console.log("Subscription Event:", req.body);
+
+res.send("Subscription Successful");
+
+});
+
+app.get("/", (req,res)=>{
+res.send("BDapps Cricket Server Running");
+})
+
+app.listen(10000, () => {
+
+console.log("Server running on port 10000");
+
+});
